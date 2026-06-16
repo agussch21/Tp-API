8940c42e5d4e8b566a012faf0eb8591ab9e499a2
Thiven-dot <tberg1514@gmail.com>
2026-06-16 03:32:01 -0300

    TurnoDTO.cs

---
+++ DTOs/TurnoDTO.cs
@@ -0,0 +1,9 @@
+namespace TurnosMedicosAPI.DTOs
+{
+    public class TurnoDTO
+    {
+        public DateTime FechaHora { get; set; }
+        public string Especialidad { get; set; }
+        public int PacienteId { get; set; }
+    }
+}
