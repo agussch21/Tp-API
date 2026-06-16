524a64d01af6c8e71ebb5e2b12bdf00efba46b5c
Thiven-dot <tberg1514@gmail.com>
2026-06-16 03:31:44 -0300

    PacienteDTO.cs

---
+++ DTOs/PacienteDTO.cs
@@ -0,0 +1,9 @@
+namespace TurnosMedicosAPI.DTOs
+{
+    public class PacienteDTO
+    {
+        public string NombreCompleto { get; set; }
+        public string DNI { get; set; }
+        public string ObraSocial { get; set; }
+    }
+}
