cc8bff99d4b9061e8d4f7191cfe15b955e046d51
Thiven-dot <tberg1514@gmail.com>
2026-06-17 15:16:07 -0300

    Update PacienteDTO.cs
    Corregi el error de conflicto a causa de que lo modificamos los 2

--- DTOs/PacienteDTO.cs
+++ DTOs/PacienteDTO.cs
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-namespace TurnosMedicosAPI.DTOs
-{
-    public class PacienteDTO
-    {
-        public int Id { get; set; }
-        public string NombreCompleto { get; set; }
-        public string DNI { get; set; }
-        public string ObraSocial { get; set; }
-    }
-
-    public class PacienteCreateDTO
-    {
-        public string NombreCompleto { get; set; }
-        public string DNI { get; set; }
-        public string ObraSocial { get; set; }
-    }
-}
-=======
 namespace TurnosMedicosAPI.DTOs
 {
     public class PacienteDTO
@@ -26,4 +7,3 @@ namespace TurnosMedicosAPI.DTOs
         public string ObraSocial { get; set; }
     }
 }
->>>>>>> e7fffa95f9c76085a333b5e4e2b89ab73e2da12b
