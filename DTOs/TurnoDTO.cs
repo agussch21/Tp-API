4c796a4c1b4415ceed7f44e0f0cab742f1197d4a
Thiven-dot <tberg1514@gmail.com>
2026-06-17 15:16:51 -0300

    Update TurnoDTO.cs
    Corregi el error de conflicto a causa de que lo modificamos los 2

--- DTOs/TurnoDTO.cs
+++ DTOs/TurnoDTO.cs
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-namespace TurnosMedicosAPI.DTOs
-{
-    public class TurnoDTO
-    {
-        public int Id { get; set; }
-        public DateTime FechaHora { get; set; }
-        public string Especialidad { get; set; }
-        public string NombrePaciente { get; set; }
-    }
-
-    public class TurnoCreateDTO
-    {
-        public DateTime FechaHora { get; set; }
-        public string Especialidad { get; set; }
-        public int PacienteId { get; set; }
-    }
-}
-=======
 namespace TurnosMedicosAPI.DTOs
 {
     public class TurnoDTO
@@ -26,4 +7,3 @@ namespace TurnosMedicosAPI.DTOs
         public int PacienteId { get; set; }
     }
 }
->>>>>>> e7fffa95f9c76085a333b5e4e2b89ab73e2da12b
