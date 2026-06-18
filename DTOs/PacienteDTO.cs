5ef5f19bc963ca3845c60f8e9119b14c020bb3f6
Thiven-dot <tberg1514@gmail.com>
2026-06-17 23:12:27 -0300

    Update PacienteDTO.cs
    Agrege el public int id

--- DTOs/PacienteDTO.cs
+++ DTOs/PacienteDTO.cs
@@ -2,6 +2,7 @@ namespace TurnosMedicosAPI.DTOs
 {
     public class PacienteDTO
     {
+        public int Id { get; set; }
         public string NombreCompleto { get; set; }
         public string DNI { get; set; }
         public string ObraSocial { get; set; }
