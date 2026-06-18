0e7ddaee71cab809257061603449599ca73f379a
Thiven-dot <tberg1514@gmail.com>
2026-06-17 23:13:18 -0300

    Update TurnoDTO.cs
    Agrege int id y string nombre paciente asi no da error

--- DTOs/TurnoDTO.cs
+++ DTOs/TurnoDTO.cs
@@ -2,8 +2,10 @@ namespace TurnosMedicosAPI.DTOs
 {
     public class TurnoDTO
     {
+        public int Id { get; set; }
         public DateTime FechaHora { get; set; }
         public string Especialidad { get; set; }
         public int PacienteId { get; set; }
+        public string NombrePaciente { get; set; }
     }
 }
