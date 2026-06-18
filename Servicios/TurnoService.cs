4d417d11bba4cb49299c738cb130b64fbb3ad988
Thiven-dot <tberg1514@gmail.com>
2026-06-17 23:16:45 -0300

    Update TurnoService.cs
    Se corrigio un error  de sintaxis y se agrego el metodo PUT

--- Servicios/TurnoService.cs
+++ Servicios/TurnoService.cs
@@ -30,7 +30,7 @@ namespace TurnosMedicosAPI.Services
                 lista.Add(new TurnoDTO
                 {
                     Id = reader.GetInt32(0),
-                    FechaHora = reader.GetString(1),
+                    FechaHora = DateTime.Parse(reader.GetString(1)),
                     Especialidad = reader.GetString(2),
                     PacienteId = reader.GetInt32(3),
                     NombrePaciente = reader.GetString(4)
@@ -51,7 +51,24 @@ namespace TurnosMedicosAPI.Services
             cmd.Parameters.AddWithValue("$pacienteId", dto.PacienteId);
             
             await cmd.ExecuteNonQueryAsync();
-        }public async Task Eliminar(int id)
+        }
+
+        public async Task Actualizar(int id, TurnoDTO dto)
+        {
+            using var cn = new SqliteConnection(_connection);
+            await cn.OpenAsync();
+            
+            using var cmd = cn.CreateCommand();
+            cmd.CommandText = "UPDATE Turnos SET FechaHora = $fecha, Especialidad = $especialidad, PacienteId = $pacienteId WHERE Id = $id;";
+            cmd.Parameters.AddWithValue("$fecha", dto.FechaHora.ToString("yyyy-MM-dd HH:mm:ss"));
+            cmd.Parameters.AddWithValue("$especialidad", dto.Especialidad);
+            cmd.Parameters.AddWithValue("$pacienteId", dto.PacienteId);
+            cmd.Parameters.AddWithValue("$id", id);
+            
+            await cmd.ExecuteNonQueryAsync();
+        }
+
+        public async Task Eliminar(int id)
         {
             using var cn = new SqliteConnection(_connection);
             await cn.OpenAsync();
