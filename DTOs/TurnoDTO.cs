a39575c8f96e02d29b7cc4cdd007bcbf94c33997
Thiven-dot <tberg1514@gmail.com>
2026-06-17 23:43:41 -0300

    Update TurnoDTO.cs

--- DTOs/TurnoDTO.cs
+++ DTOs/TurnoDTO.cs
@@ -3,7 +3,7 @@ namespace TurnosMedicosAPI.DTOs
     public class TurnoDTO
     {
         public int Id { get; set; }
-        public DateTime FechaHora { get; set; }
+        public string FechaHora { get; set; } // Cambiado de DateTime a string
         public string Especialidad { get; set; }
         public int PacienteId { get; set; }
         public string NombrePaciente { get; set; }
